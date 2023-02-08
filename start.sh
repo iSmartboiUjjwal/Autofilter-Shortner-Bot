8087d5c5409374b7391000cf3501f359a60ba873
iSmartboiUjjwal <122801676+iSmartboiUjjwal@users.noreply.github.com>
2023-02-08 19:30:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
-fi
+echo "Cloning Custom Repo from $UPSTREAM_REPO "
+git clone $UPSTREAM_REPO /EvaMaria
 cd /EvaMaria
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
