038f7e66eef320cf30758e73ea1710207519be68
iSmartboiUjjwal <122801676+iSmartboiUjjwal@users.noreply.github.com>
2023-02-08 22:29:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,5 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
 fi
-cd /EvaMaria
-pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
