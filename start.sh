2f55904b0f897b5983b2d96378fda4cf9d245dcd
iSmartboiUjjwal <122801676+iSmartboiUjjwal@users.noreply.github.com>
2023-02-09 15:11:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/iSmartboiUjjwal/Autofilter-and-Shortner-bot-.git /EvaMaria
+  git clone https://github.com/iSmartboiUjjwal/Autofilter-and-Shortner-bot.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
